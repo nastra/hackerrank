d4a940350c27b4c1425f9eb80194650d193bcb9f
Eduard Tudenhöfner <etudenhoefner@gmail.com>
2014-02-01 11:27:23 +0100

    added solution to the harry potter floating rocks problem

---
+++ src/main/python/harry_potter_floating_rocks.py
@@ -0,0 +1,30 @@
+from fractions import gcd
+
+__author__ = 'nastra'
+
+"""
+See https://www.hackerrank.com/contests/101jan14/challenges/harry-potter-and-the-floating-rocks for problem statement
+"""
+
+
+def gcd(a, b):
+    if b == 0:
+        return a
+    else:
+        return gcd(b, a % b)
+
+
+if __name__ == '__main__':
+    #from fractions import gcd
+
+    n = int(raw_input())
+
+    sum = 0
+    for x in xrange(0, n):
+        line = raw_input().split()
+        x1 = int(line[0])
+        y1 = int(line[1])
+        x2 = int(line[2])
+        y2 = int(line[3])
+        result = gcd(abs(x2 - x1), abs(y2 - y1)) - 1
+        print(result)
