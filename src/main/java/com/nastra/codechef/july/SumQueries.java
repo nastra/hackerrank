73a81a55e9b6a526e6615d4f6fcf8f572727b857
Eduard Tudenhöfner <etudenhoefner@gmail.com>
2014-07-20 21:35:39 +0200

    added two solutions from the problems of July Cook-Off

---
+++ src/main/java/com/nastra/codechef/july/SumQueries.java
@@ -0,0 +1,151 @@
+package com.nastra.codechef.july;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.math.BigInteger;
+import java.util.StringTokenizer;
+
+/**
+ * @see http://www.codechef.com/COOK48/problems/RRSUM
+ * @author nastra
+ * 
+ */
+public class SumQueries {
+
+    public static long solve(BigInteger N, BigInteger mid, BigInteger max, long x) {
+        BigInteger result = BigInteger.ZERO;
+        BigInteger X = BigInteger.valueOf(x);
+        if (X.compareTo(mid) < 0) {
+            result = X.subtract(N.add(BigInteger.ONE));
+        } else if (X.compareTo(mid) == 0) {
+            result = N;
+        } else {
+            result = max.subtract(X).add(BigInteger.ONE);
+        }
+        if (result.compareTo(BigInteger.ZERO) <= 0) {
+            return 0;
+        }
+        return result.longValue();
+    }
+
+    public static void main(String[] args) throws Exception {
+        FastScanner sc = new FastScanner(System.in);
+        PrintWriter out = new PrintWriter(System.out);
+        int N = sc.nextInt();
+        int Q = sc.nextInt();
+        BigInteger mid = BigInteger.valueOf(Long.valueOf(2 * N + 1));
+        BigInteger n = BigInteger.valueOf(Long.valueOf(N));
+        BigInteger max = BigInteger.valueOf(2 * N).add(n);
+        while (Q > 0) {
+            Q--;
+            out.println(solve(n, mid, max, sc.nextLong()));
+        }
+        out.close();
+    }
+
+    static class FastScanner {
+
+        private static BufferedReader reader;
+        private static StringTokenizer tokenizer;
+
+        public FastScanner(InputStream in) throws Exception {
+            reader = new BufferedReader(new InputStreamReader(in));
+            tokenizer = new StringTokenizer(reader.readLine().trim());
+        }
+
+        public int numTokens() throws Exception {
+            if (!tokenizer.hasMoreTokens()) {
+                tokenizer = new StringTokenizer(reader.readLine().trim());
+                return numTokens();
+            }
+            return tokenizer.countTokens();
+        }
+
+        public boolean hasNext() throws Exception {
+            if (!tokenizer.hasMoreTokens()) {
+                tokenizer = new StringTokenizer(reader.readLine().trim());
+                return hasNext();
+            }
+            return true;
+        }
+
+        public String next() throws Exception {
+            if (!tokenizer.hasMoreTokens()) {
+                tokenizer = new StringTokenizer(reader.readLine().trim());
+                return next();
+            }
+            return tokenizer.nextToken();
+        }
+
+        public double nextDouble() throws Exception {
+            return Double.parseDouble(next());
+        }
+
+        public float nextFloat() throws Exception {
+            return Float.parseFloat(next());
+        }
+
+        public long nextLong() throws Exception {
+            return Long.parseLong(next());
+        }
+
+        public int nextInt() throws Exception {
+            return Integer.parseInt(next());
+        }
+
+        public int[] nextIntArray() throws Exception {
+            String[] line = reader.readLine().trim().split(" ");
+            int[] out = new int[line.length];
+            for (int i = 0; i < line.length; i++) {
+                out[i] = Integer.valueOf(line[i]);
+            }
+            return out;
+        }
+
+        public double[] nextDoubleArray() throws Exception {
+            String[] line = reader.readLine().trim().split(" ");
+            double[] out = new double[line.length];
+            for (int i = 0; i < line.length; i++) {
+                out[i] = Double.valueOf(line[i]);
+            }
+            return out;
+        }
+
+        public Integer[] nextIntegerArray() throws Exception {
+            String[] line = reader.readLine().trim().split(" ");
+            Integer[] out = new Integer[line.length];
+            for (int i = 0; i < line.length; i++) {
+                out[i] = Integer.valueOf(line[i]);
+            }
+            return out;
+        }
+
+        public BigInteger[] nextBigIngtegerArray() throws Exception {
+            String[] line = reader.readLine().trim().split(" ");
+            BigInteger[] out = new BigInteger[line.length];
+            for (int i = 0; i < line.length; i++) {
+                out[i] = new BigInteger(line[i]);
+            }
+            return out;
+        }
+
+        public BigInteger nextBigInteger() throws Exception {
+            return new BigInteger(next());
+        }
+
+        public String nextLine() throws Exception {
+            return reader.readLine().trim();
+        }
+
+        public long[] nextLongArray() throws Exception {
+            String[] line = reader.readLine().trim().split(" ");
+            long[] out = new long[line.length];
+            for (int i = 0; i < line.length; i++) {
+                out[i] = Long.valueOf(line[i]);
+            }
+            return out;
+        }
+    }
+}
