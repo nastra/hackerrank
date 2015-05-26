35b484d95b70fbe4c58f9a9357b34f0f47e63f24
Eduard Tudenhoefner <eduard.tudenhoefner@datastax.com>
2015-05-26 10:44:24 +0200

    Added solution to ProjectEuler #1

---
+++ src/main/java/euler/No1.java
@@ -0,0 +1,137 @@
+package euler;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.math.BigInteger;
+import java.util.StringTokenizer;
+
+public class No1 {
+
+	private static long sum(long n) {
+		return n * (n + 1) / 2;
+	}
+
+	public static long solve(int n) {
+		n--;
+		return sum(n / 3) * 3 + sum(n / 5) * 5 - sum(n / 15)*15;
+	}
+
+	public static void main(String[] args) throws Exception {
+		FastScanner sc = new FastScanner(System.in);
+		PrintWriter out = new PrintWriter(System.out);
+		int t = sc.nextInt();
+		while (t > 0) {
+			t--;
+			out.println(solve(sc.nextInt()));
+		}
+
+		out.close();
+	}
+
+	private static class FastScanner {
+
+		private static BufferedReader reader;
+		private static StringTokenizer tokenizer;
+
+		public FastScanner(InputStream in) throws Exception {
+			reader = new BufferedReader(new InputStreamReader(in));
+			tokenizer = new StringTokenizer(reader.readLine().trim());
+		}
+
+		public int numTokens() throws Exception {
+			if (!tokenizer.hasMoreTokens()) {
+				tokenizer = new StringTokenizer(reader.readLine().trim());
+				return numTokens();
+			}
+			return tokenizer.countTokens();
+		}
+
+		public boolean hasNext() throws Exception {
+			if (!tokenizer.hasMoreTokens()) {
+				tokenizer = new StringTokenizer(reader.readLine().trim());
+				return hasNext();
+			}
+			return true;
+		}
+
+		public String next() throws Exception {
+			if (!tokenizer.hasMoreTokens()) {
+				tokenizer = new StringTokenizer(reader.readLine().trim());
+				return next();
+			}
+			return tokenizer.nextToken();
+		}
+
+		public double nextDouble() throws Exception {
+			return Double.parseDouble(next());
+		}
+
+		public float nextFloat() throws Exception {
+			return Float.parseFloat(next());
+		}
+
+		public long nextLong() throws Exception {
+			return Long.parseLong(next());
+		}
+
+		public int nextInt() throws Exception {
+			return Integer.parseInt(next());
+		}
+
+		public int[] nextIntArray() throws Exception {
+			String[] line = reader.readLine().trim().split(" ");
+			int[] out = new int[line.length];
+			for (int i = 0; i < line.length; i++) {
+				out[i] = Integer.valueOf(line[i]);
+			}
+			return out;
+		}
+
+		public double[] nextDoubleArray() throws Exception {
+			String[] line = reader.readLine().trim().split(" ");
+			double[] out = new double[line.length];
+			for (int i = 0; i < line.length; i++) {
+				out[i] = Double.valueOf(line[i]);
+			}
+			return out;
+		}
+
+		public Integer[] nextIntegerArray() throws Exception {
+			String[] line = reader.readLine().trim().split(" ");
+			Integer[] out = new Integer[line.length];
+			for (int i = 0; i < line.length; i++) {
+				out[i] = Integer.valueOf(line[i]);
+			}
+			return out;
+		}
+
+		public BigInteger[] nextBigIntegerArray() throws Exception {
+			String[] line = reader.readLine().trim().split(" ");
+			BigInteger[] out = new BigInteger[line.length];
+			for (int i = 0; i < line.length; i++) {
+				out[i] = new BigInteger(line[i]);
+			}
+			return out;
+		}
+
+		public BigInteger nextBigInteger() throws Exception {
+			return new BigInteger(next());
+		}
+
+		public String nextLine() throws Exception {
+			return reader.readLine().trim();
+		}
+
+		public long[] nextLongArray() throws Exception {
+			String[] line = reader.readLine().trim().split(" ");
+			long[] out = new long[line.length];
+			for (int i = 0; i < line.length; i++) {
+				out[i] = Long.valueOf(line[i]);
+			}
+			return out;
+		}
+	}
+}
+
